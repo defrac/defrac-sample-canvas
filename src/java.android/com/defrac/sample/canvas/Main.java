7ec268b48aebf0d18ecf0cee615bb108fb8ef528
Joa Ebert <joa.ebert@defrac.com>
2015-06-09 13:23:10 +0200

    Add iOS and Android entry point

--- src/java.android/com/defrac/sample/canvas/Main.java
+++ src/java.android/com/defrac/sample/canvas/Main.java
@@ -1,16 +1,19 @@
 package com.defrac.sample.canvas;
 
-import javax.annotation.Nonnull;
+import android.content.Intent;
+import android.os.Build;
+import com.defrac.sample.canvas.ScribblerScreen;
+import defrac.dni.Activity;
+import defrac.dni.IntentFilter;
+import defrac.dni.UsesSdk;
+import defrac.ui.Screen;
+import defrac.ui.ScreenActivity;
 
-/**
- *
- */
-public final class Main {
-  @Nonnull
-  public static Main create() {
-    return new Main();
-  }
-
-  private Main() {
+@Activity(label = "Canvas", filter = @IntentFilter(action = Intent.ACTION_MAIN, category = Intent.CATEGORY_LAUNCHER))
+@UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public final class Main extends ScreenActivity {
+  @Override
+  protected Screen createScreen() {
+    return new ScribblerScreen();
   }
 }
--- src/java.ios/com/defrac/sample/canvas/Main.java
+++ src/java.ios/com/defrac/sample/canvas/Main.java
@@ -1,16 +1,22 @@
 package com.defrac.sample.canvas;
 
+import defrac.app.ScreenAppDelegate;
+import defrac.ios.uikit.UIKit;
+import defrac.ui.Screen;
+
 import javax.annotation.Nonnull;
 
 /**
  *
  */
-public final class Main {
-  @Nonnull
-  public static Main create() {
-    return new Main();
+public final class Main extends ScreenAppDelegate {
+  public static void main(String[] args) {
+    UIKit.applicationMain(args, null, Main.class);
   }
 
-  private Main() {
+  @Nonnull
+  @Override
+  protected Screen createScreen() {
+    return new ScribblerScreen();
   }
 }
