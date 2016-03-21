985adc77cb7e39dcfec045b2b0fc1627ac00afdf
Joa Ebert <joa.ebert@defrac.com>
2016-03-21 14:22:25 +0100

    Update for 0.3.116

--- src/java.android/com/defrac/sample/canvas/Main.java
+++ src/java.android/com/defrac/sample/canvas/Main.java
@@ -5,12 +5,10 @@ import android.os.Build;
 import com.defrac.sample.canvas.ScribblerScreen;
 import defrac.dni.Activity;
 import defrac.dni.IntentFilter;
-import defrac.dni.UsesSdk;
 import defrac.ui.Screen;
 import defrac.ui.ScreenActivity;
 
 @Activity(label = "Canvas", filter = @IntentFilter(action = Intent.ACTION_MAIN, category = Intent.CATEGORY_LAUNCHER))
-@UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public final class Main extends ScreenActivity {
   @Override
   protected Screen createScreen() {
